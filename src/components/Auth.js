66605288b1cc068d875b3feb709d0e4a564eec6b
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2023-04-17 13:22:02 +0200

    fixed env

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -10,8 +10,8 @@ export default function Auth() {
 
     const getURL = () => {
       let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? 
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 
+        process.env.NEXT_PUBLIC_SITE_URL ?? 
+        process.env.NEXT_PUBLIC_VERCEL_URL ?? 
         'http://localhost:3000/';
 
       url = url.includes('http') ? url : `https://${url}`;
